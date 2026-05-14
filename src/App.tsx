6157f03fffaf682cf08aa78f4c98168de525b20e
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-14 08:03:36 -0500

    temp-check

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import FeedbackWidget from './components/FeedbackWidget'
 import Home from './pages/Home'
-import WhyMonarchs from './pages/WhyMonarchs'
-import PlantMilkweed from './pages/PlantMilkweed'
-import TheMigration from './pages/TheMigration'
-import TakeAction from './pages/TakeAction'
-import ChooseAPlant from './pages/ChooseAPlant'
-import NativePlantNearMe from './pages/NativePlantNearMe'
 import MonarchLife from './pages/MonarchLife'
+import PlantMilkweed from './pages/PlantMilkweed'
 import RaiseAMonarch from './pages/RaiseAMonarch'
-import TheFirstYear from './pages/TheFirstYear'
+import TheMigration from './pages/TheMigration'
+import WhyMonarchs from './pages/WhyMonarchs'
+import ChooseAPlant from './pages/ChooseAPlant'
+import TakeAction from './pages/TakeAction'
+import NativePlantNearMe from './pages/NativePlantNearMe'
 import FAQ from './pages/FAQ'
 import LogASighting from './pages/LogASighting'
 import WaystationGuide from './pages/WaystationGuide'
+import TalkingPoints from './pages/TalkingPoints'
+import TheFirstYear from './pages/TheFirstYear'
 import Fireflies from './pages/Fireflies'
 import BoxTurtles from './pages/BoxTurtles'
 import NativeBees from './pages/NativeBees'
@@ -46,91 +47,5 @@ import HabitatScore from './pages/HabitatScore'
 import SpringChecklist from './pages/SpringChecklist'
 import PesticideGuide from './pages/PesticideGuide'
 import WaterForWildlife from './pages/WaterForWildlife'
+import LeaveTheLeaves from './pages/LeaveTheLeaves'
 import { trackEvent } from './lib/analytics'
-
-export type Page = 'home' | 'why-monarchs' | 'plant-milkweed' | 'the-migration' | 'take-action' | 'choose-a-plant' | 'native-plant-near-me' | 'monarch-life' | 'raise-a-monarch' | 'the-first-year' | 'faq' | 'log-a-sighting' | 'waystation-guide' | 'fireflies' | 'box-turtles' | 'native-bees' | 'talking-points' | 'gray-tree-frogs' | 'downy-woodpecker' | 'baltimore-oriole' | 'northern-cardinal' | 'eastern-bluebird' | 'black-capped-chickadee' | 'ruby-throated-hummingbird'
-  | 'american-goldfinch'
-  | 'american-robin'
-  | 'indigo-bunting'
-  | 'tiger-swallowtail'
-  | 'green-darner'
-  | 'garter-snake'
-  | 'dark-eyed-junco'
-  | 'eastern-chipmunk'
-  | 'spring-peeper'
-  | 'toad'
-  | 'seasonal-calendar'
-  | 'species-gallery'
-  | 'habitat-transformation'
-  | 'hoa-guide'
-  | 'habitat-score'
-  | 'spring-checklist'
-  | 'pesticide-guide'
-  | 'water-for-wildlife'
-
-function App() {
-  const [page, setPage] = useState<Page>('home')
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'instant' })
-    trackEvent('page_view', page)
-  }, [page])
-
-  const renderPage = () => {
-    switch (page) {
-      case 'why-monarchs':        return <WhyMonarchs onNavigate={setPage} />
-      case 'plant-milkweed':      return <PlantMilkweed onNavigate={setPage} />
-      case 'the-migration':       return <TheMigration onNavigate={setPage} />
-      case 'take-action':         return <TakeAction onNavigate={setPage} />
-      case 'choose-a-plant':      return <ChooseAPlant onNavigate={setPage} />
-      case 'native-plant-near-me': return <NativePlantNearMe onNavigate={setPage} />
-      case 'monarch-life':        return <MonarchLife onNavigate={setPage} />
-      case 'raise-a-monarch':     return <RaiseAMonarch onNavigate={setPage} />
-      case 'the-first-year':      return <TheFirstYear onNavigate={setPage} />
-      case 'faq':                  return <FAQ onNavigate={setPage} />
-      case 'log-a-sighting':      return <LogASighting onNavigate={setPage} />
-      case 'waystation-guide':    return <WaystationGuide onNavigate={setPage} />
-      case 'fireflies':           return <Fireflies onNavigate={setPage} />
-      case 'box-turtles':         return <BoxTurtles onNavigate={setPage} />
-      case 'native-bees':         return <NativeBees onNavigate={setPage} />
-      case 'talking-points':      return <TalkingPoints onNavigate={setPage} />
-      case 'gray-tree-frogs':     return <GrayTreeFrogs onNavigate={setPage} />
-      case 'downy-woodpecker':    return <DownyWoodpecker onNavigate={setPage} />
-      case 'baltimore-oriole':    return <BaltimoreOriole onNavigate={setPage} />
-      case 'northern-cardinal':   return <NorthernCardinal onNavigate={setPage} />
-      case 'eastern-bluebird':    return <EasternBluebird onNavigate={setPage} />
-      case 'black-capped-chickadee': return <BlackCappedChickadee onNavigate={setPage} />
-      case 'ruby-throated-hummingbird': return <RubyThroatedHummingbird onNavigate={setPage} />
-      case 'american-goldfinch': return <AmericanGoldfinch onNavigate={setPage} />
-      case 'american-robin': return <AmericanRobin onNavigate={setPage} />
-      case 'indigo-bunting': return <IndigoBunting onNavigate={setPage} />
-      case 'tiger-swallowtail': return <TigerSwallowtail onNavigate={setPage} />
-      case 'green-darner': return <GreenDarner onNavigate={setPage} />
-      case 'garter-snake': return <GarterSnake onNavigate={setPage} />
-      case 'dark-eyed-junco': return <DarkEyedJunco onNavigate={setPage} />
-      case 'eastern-chipmunk': return <EasternChipmunk onNavigate={setPage} />
-      case 'spring-peeper': return <SpringPeeper onNavigate={setPage} />
-      case 'toad': return <AmericanToad onNavigate={setPage} />
-      case 'seasonal-calendar': return <SeasonalCalendar onNavigate={setPage} />
-      case 'species-gallery': return <SpeciesGallery onNavigate={setPage} />
-      case 'habitat-transformation': return <HabitatTransformation onNavigate={setPage} />
-      case 'hoa-guide': return <HOAGuide onNavigate={setPage} />
-      case 'habitat-score':        return <HabitatScore onNavigate={setPage} />
-      case 'spring-checklist':    return <SpringChecklist onNavigate={setPage} />
-      case 'pesticide-guide':     return <PesticideGuide onNavigate={setPage} />
-      case 'water-for-wildlife':  return <WaterForWildlife onNavigate={setPage} />
-      default:                    return <Home onNavigate={setPage} />
-    }
-  }
-
-  return (
-    <I18nextProvider i18n={i18n}>
-      <Nav current={page} onNavigate={setPage} />
-      <main>{renderPage()}</main>
-      <Footer onNavigate={setPage} />
-      <FeedbackWidget page={page} />
-    </I18nextProvider>
-  )
-}
-
-export default App
