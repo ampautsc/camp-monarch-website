38cb1997364551a59a119bc07dc75fb70172ea83
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-18 14:30:29 -0500

    feat: wire BlackEyedSusan in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -91,6 +91,7 @@ import SwampMilkweed from './pages/SwampMilkweed'
 import WildBergamot from './pages/WildBergamot'
 import NewJerseyTea from './pages/NewJerseyTea'
 import WildColumbine from './pages/WildColumbine'
+import BlackEyedSusan from './pages/BlackEyedSusan'
 import SeasonalCalendar from './pages/SeasonalCalendar'
 import SpeciesGallery from './pages/SpeciesGallery'
 import HabitatTransformation from './pages/HabitatTransformation'
@@ -226,6 +227,7 @@ export default function App() {
       case 'wild-bergamot':    return <WildBergamot onNavigate={navigateTo} />
       case 'new-jersey-tea':   return <NewJerseyTea onNavigate={navigateTo} />
       case 'wild-columbine':   return <WildColumbine onNavigate={navigateTo} />
+      case 'black-eyed-susan': return <BlackEyedSusan onNavigate={navigateTo} />
       case 'seasonal-calendar': return <SeasonalCalendar onNavigate={navigateTo} />
       case 'species-gallery': return <SpeciesGallery onNavigate={navigateTo} />
       case 'habitat-transformation': return <HabitatTransformation onNavigate={navigateTo} />
