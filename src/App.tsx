652f3d7fb4779f9fbfe216d08b8302bb8beb4011
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-18 15:11:01 -0500

    feat: wire PartridgePea into App.tsx routing

--- src/App.tsx
+++ src/App.tsx
@@ -95,6 +95,7 @@ import BlackEyedSusan from './pages/BlackEyedSusan'
 import VirginiaBluebell from './pages/VirginiaBluebell'
 import Buttonbush from './pages/Buttonbush'
 import Ironweed from './pages/Ironweed'
+import PartridgePea from './pages/PartridgePea'
 import SeasonalCalendar from './pages/SeasonalCalendar'
 import SpeciesGallery from './pages/SpeciesGallery'
 import HabitatTransformation from './pages/HabitatTransformation'
@@ -234,6 +235,7 @@ export default function App() {
       case 'virginia-bluebell': return <VirginiaBluebell onNavigate={navigateTo} />
       case 'buttonbush':       return <Buttonbush onNavigate={navigateTo} />
       case 'ironweed':         return <Ironweed onNavigate={navigateTo} />
+      case 'partridge-pea':    return <PartridgePea onNavigate={navigateTo} />
       case 'seasonal-calendar': return <SeasonalCalendar onNavigate={navigateTo} />
       case 'species-gallery': return <SpeciesGallery onNavigate={navigateTo} />
       case 'habitat-transformation': return <HabitatTransformation onNavigate={navigateTo} />
