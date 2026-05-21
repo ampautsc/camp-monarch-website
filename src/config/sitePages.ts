fcfa518faa1c78ec6d80aeecaf6e85384d09a596
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-21 16:44:31 -0500

    feat: add white-oak slug to sitePages.ts

--- src/config/sitePages.ts
+++ src/config/sitePages.ts
@@ -139,6 +139,7 @@ export const ALL_PAGE_SLUGS = [
   'little-bluestem',
   'spicebush-swallowtail',
   'hummingbird-clearwing-moth',
+  'white-oak',
 ] as const
 
 export type PageSlug = typeof ALL_PAGE_SLUGS[number]
