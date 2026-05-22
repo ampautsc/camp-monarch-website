9e91d7d5a48053daa464a80bd3a2fe7496f5202b
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-22 01:15:21 -0500

    feat: add MourningCloak import and route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -136,6 +136,7 @@ import PearlCrescent from './pages/PearlCrescent'
 import GiantSwallowtail from './pages/GiantSwallowtail'
 import RedAdmiral from './pages/RedAdmiral'
 import PaintedLady from './pages/PaintedLady'
+import MourningCloak from './pages/MourningCloak'
 
 import { ALL_PAGE_SLUGS, type PageSlug } from './config/sitePages'
 export type Page = PageSlug
@@ -286,6 +287,7 @@ export default function App() {
       case 'giant-swallowtail': return <GiantSwallowtail onNavigate={navigateTo} />
       case 'red-admiral':       return <RedAdmiral onNavigate={navigateTo} />
       case 'painted-lady':      return <PaintedLady onNavigate={navigateTo} />
+      case 'mourning-cloak':    return <MourningCloak onNavigate={navigateTo} />
       case 'seasonal-calendar': return <SeasonalCalendar onNavigate={navigateTo} />
       case 'species-gallery': return <SpeciesGallery onNavigate={navigateTo} />
       case 'habitat-transformation': return <HabitatTransformation onNavigate={navigateTo} />
