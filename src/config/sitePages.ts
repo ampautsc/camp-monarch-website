4131f32d9e5886633c363254b3160fd8ab3f3da3
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-25 04:26:41 -0500

    feat: add zebra-swallowtail to sitePages.ts

--- src/config/sitePages.ts
+++ src/config/sitePages.ts
@@ -171,6 +171,7 @@ export const ALL_PAGE_SLUGS = [
   'gray-hairstreak',
   'sleepy-orange',
   'little-yellow',
+  'zebra-swallowtail',
 ] as const
 
 export type PageSlug = typeof ALL_PAGE_SLUGS[number]
