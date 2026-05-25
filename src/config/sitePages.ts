2026b271aa218788e3d0bc35d6d978309338b491
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-05-24 22:11:38 -0500

    feat: add silvery-checkerspot slug to sitePages.ts

--- src/config/sitePages.ts
+++ src/config/sitePages.ts
@@ -158,6 +158,7 @@ export const ALL_PAGE_SLUGS = [
   'cloudless-sulphur',
   'american-lady',
   'common-buckeye',
+  'silvery-checkerspot',
 ] as const
 
 export type PageSlug = typeof ALL_PAGE_SLUGS[number]
