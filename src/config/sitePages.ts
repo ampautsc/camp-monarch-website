c642a9eb9f4314e69f31a805fd8b6cd00c8e0f3e
ampautsc <84487549+ampautsc@users.noreply.github.com>
2026-06-01 18:55:47 -0500

    feat: add american-woodcock to ALL_PAGE_SLUGS

--- src/config/sitePages.ts
+++ src/config/sitePages.ts
@@ -192,6 +192,7 @@ export const ALL_PAGE_SLUGS = [
   'eastern-cottontail',
   'striped-skunk',
   'red-fox',
+  'american-woodcock',
 ] as const
 
 export type PageSlug = typeof ALL_PAGE_SLUGS[number]
